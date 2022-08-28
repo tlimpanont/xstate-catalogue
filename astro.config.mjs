a7c3b750ad1a3b81593dac621a326451f8f0c037
Theuy Limpanont <info@theuy.nl>
2022-08-28 14:00:39 +0200

    feat(app): github pages ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  site: 'https://tlimpanont.github.io',
+  base: '/xstate-catalogue',
 });
